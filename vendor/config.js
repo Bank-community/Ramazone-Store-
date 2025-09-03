0ea1395be24393533a53caafba2d1ca334c8a069
Prince rama🤴 <princekumar954684@gmail.com>
2025-09-03 16:43:51 +0530

    Create config.js

---
+++ vendor/config.js
@@ -0,0 +1,11 @@
+// vendor/config.js
+// Yahaan apni saari API keys rakhein.
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCXrwTUdy5B5mxEMsmAOX_3ZVKxiWht7Vw", // <-- Apni asli key yahaan daalein
+  authDomain: "re-store-8e5b3.firebaseapp.com",
+  databaseURL: "https://re-store-8e5b3-default-rtdb.asia-southeast1.firebasedatabase.app",
+};
+
+const IMGBB_API_KEY = "f513510bd9ce285f80f9df4d3648451a"; // <-- Apni asli ImgBB key yahaan daalein
+
