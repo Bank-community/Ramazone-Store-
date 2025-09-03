1073f25ee8beadb1b633227dab815d06ae990a57
Prince rama🤴 <princekumar954684@gmail.com>
2025-09-03 15:40:49 +0530

    Update get-vendor-firebase.js

--- api/get-vendor-firebase.js
+++ api/get-vendor-firebase.js
@@ -10,15 +10,5 @@ export default function handler(request, response) {
     databaseURL: process.env.VENDOR_FIREBASE_DATABASE_URL, // <-- NAAM BADAL GAYA HAI
   };
 
-  // Check karein ki Vercel se saari keys mil rahi hain ya nahi
-  if (!firebaseConfig.apiKey || !firebaseConfig.authDomain || !firebaseConfig.databaseURL) {
-    return response.status(500).json({ 
-      error: "Server par VENDOR_FIREBASE... naam ke environment variables set nahi hain." 
-    });
-  }
-
-  // Sahi config ko bhejein
-  response.status(200).json(firebaseConfig);
-}
-
+  
 
